a104adeb9b2534172b766eff6fee82d8fdb5f9a4
ZeroQ4 <dotss87@gmail.com>
2022-12-14 17:52:23 +0200

    master

--- src/components/HelpUrls.ts
+++ src/components/HelpUrls.ts
@@ -1,11 +1,11 @@
 export const helpUrls = {
-  customerSupport: 'https://t.me/ProjectSerum',
-  customerSupportZh: 'https://t.me/ProjectSerum_Chinese',
-  contactEmail: 'mailto:contact@projectserum.com',
-  discord: 'https://discord.gg/EDvudv6',
-  telegram: 'https://t.me/ProjectSerum',
-  github: 'https://github.com/project-serum',
+  customerSupport: 'https://t.me/onespaceEN',
+  customerSupportZh: 'https://t.me/onespaceEN',
+  contactEmail: 'mailto:team@1space.me',
+  discord: 'https://discord.com/invite/RacHhzcS7U',
+  telegram: 'https://t.me/onespaceEN',
+  github: 'https://github.com/Onespace-Project',
   projectSerum: 'https://projectserum.com/',
-  developerResources: 'https://projectserum.com/developer-resources',
+  developerResources: 'https://github.com/openbook-dex',
   solanaBeach: 'https://solanabeach.io',
 };
